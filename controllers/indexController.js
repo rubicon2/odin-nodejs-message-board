a51184ced81b77e19d35c624599c430df557ae77
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2024-08-19 17:05:58 +0100

    Add ability to view individual messages

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,10 +1,14 @@
+const { v4: uuid } = require('uuid');
+
 const messages = [
   {
+    id: uuid(),
     text: 'Hi there!',
     user: 'Jacqueline',
     added: new Date(),
   },
   {
+    id: uuid(),
     text: 'I have no interest in you whatsoever.',
     user: 'Miho',
     added: new Date(),
@@ -15,14 +19,24 @@ function getIndex(req, res) {
   res.render('index', { messages, title: 'Mini Messageboard' });
 }
 
+function getMessage(req, res, next) {
+  const { id } = req.params;
+  try {
+    const message = messages.find((currentMsg) => id === currentMsg.id);
+    res.render('message', { message, title: `Message by ${message.user}` });
+  } catch (error) {
+    next(error);
+  }
+}
+
 function getNew(req, res) {
   res.render('form', { title: 'Mini Messageboard' });
 }
 
 function postNew(req, res) {
   const { user, text } = req.body;
-  messages.push({ user, text, added: new Date() });
+  messages.push({ id: uuid(), user, text, added: new Date() });
   res.redirect('/');
 }
 
-module.exports = { getIndex, getNew, postNew };
+module.exports = { getIndex, getMessage, getNew, postNew };
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -7,5 +7,6 @@ router.use(express.urlencoded({ extended: true }));
 router.get('/', controller.getIndex);
 router.get('/new', controller.getNew);
 router.post('/new', controller.postNew);
+router.get('/messages/:id', controller.getMessage);
 
 module.exports = router;
