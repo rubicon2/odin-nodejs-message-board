541ed8edb7f268dba925fda28084cbfe34065d66
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2024-08-19 17:10:50 +0100

    Add error handling and 404 handling

--- app.js
+++ app.js
@@ -8,4 +8,18 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use('/', indexRouter);
+
+// Error handling.
+app.use((error, req, res, next) => {
+  console.error(error);
+  res.status(500).render('error', { error, title: 'Error' });
+});
+
+// 404.
+app.use((req, res) => {
+  res
+    .status(404)
+    .render('error', { error: '404: Page not found', title: 'Error' });
+});
+
 app.listen(PORT);
