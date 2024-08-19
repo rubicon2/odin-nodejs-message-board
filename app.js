e1528e801a21706fc930894a4555919b6ec46a0a
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2024-08-19 15:00:43 +0100

    Print messages to user on index page

--- app.js
+++ app.js
@@ -6,5 +6,7 @@ const indexRouter = require('./routes/indexRouter');
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
 app.use('/', indexRouter);
 app.listen(PORT);
--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,5 +1,18 @@
+const messages = [
+  {
+    text: 'Hi there!',
+    user: 'Jacqueline',
+    added: new Date(),
+  },
+  {
+    text: 'I have no interest in you whatsoever.',
+    user: 'Miho',
+    added: new Date(),
+  },
+];
+
 function getIndex(req, res) {
-  res.send('Got index!');
+  res.render('index', { messages, title: 'Mini Messageboard' });
 }
 
 function getNew(req, res) {
