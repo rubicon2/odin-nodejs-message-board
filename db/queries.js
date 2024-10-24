fe12c5db4e974871308285947dadc9362d097477
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2024-10-24 15:26:54 +0100

    Order messages from newest to oldest

--- db/queries.js
+++ db/queries.js
@@ -13,7 +13,9 @@ async function getMessage(id) {
 
 async function getAllMessages() {
   try {
-    const { rows } = await pool.query('SELECT * FROM messages');
+    const { rows } = await pool.query(
+      'SELECT * FROM messages ORDER BY added DESC',
+    );
     return rows;
   } catch (error) {
     console.error(error);
